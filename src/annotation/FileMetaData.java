52eabbdf43061c98a968f113c5b61a71ed1c2ed8
Apada29 <33749198+Apada29@users.noreply.github.com>
2017-11-17 14:04:03 +0200

    Update FileMetaData.java

--- src/annotation/FileMetaData.java
+++ src/annotation/FileMetaData.java
@@ -1,7 +1,10 @@
 package annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * FileMetaData annotation.
@@ -10,6 +13,8 @@ import java.lang.annotation.RetentionPolicy;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
 public @interface FileMetaData {
 
 	String separator() default ",";
