7955b86e03cc9142f94c5222663b3454fc4fbe83
Apada29 <33749198+Apada29@users.noreply.github.com>
2017-11-17 14:03:15 +0200

    Update CSVColumn.java

--- src/annotation/CSVColumn.java
+++ src/annotation/CSVColumn.java
@@ -1,7 +1,10 @@
 package annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * CSVColumn annotation.
@@ -10,6 +13,8 @@ import java.lang.annotation.RetentionPolicy;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
 public @interface CSVColumn {
 
 	int indx() default 1;
